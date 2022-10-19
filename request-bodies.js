f486a23e29749295e952a74a48db46b306fdad81
Griffin <griffinlebeau2@gmail.com>
2022-10-18 23:12:06 -0500

    more comments

--- request-bodies.js
+++ request-bodies.js
@@ -16,7 +16,7 @@ const getDeactivatedProps = property => { //callback for filter method on array 
 const getMatchingUser = user => { //callback for filter method on array of users from JSON file
     let hasDeactivatedProperty = false; //variable that deteremines whether user object is added to new filtered array or not
     let accounts = user.associated-accounts; //renaming object field for readability in for loop
-    if (!hasDeactivatedProperty){ //will always run because it is false by default, 
+    if (!hasDeactivatedProperty){ //will always run because it is false by default
         for (let i = 0; i < accounts.length; i++){//loop through each property account linked to user 
             let match = deactivatedProps.includes(accounts[i].toString()); //checks if a property account number is present in list of deactivated properties
             if (match){ //if the user has a deactivated property, set filter return variable to true to add the user instance to array for deposit
@@ -26,14 +26,14 @@ const getMatchingUser = user => { //callback for filter method on array of users
             };
         };
     };
-    return hasDeactivatedProperty; //controls whether 
+    return hasDeactivatedProperty; //returning true adds array element to new list, returning false does not
 };
 
 const populateBodies = deactivatedList => { //takes in array of users linked to a deactivated property
     deactivatedList.forEach(function(user){ //transforms each user into form ready for api call request body 
         emailList.push(user.email); //populates email array for contact list update api call
         let newUser = { //object matching api syntax 
-            email: user.email, 
+            email: user.email, //opted for email vs id due to already having email available in JSON file vs waiting for vid from api call
             properties: [
                 { property: 'first_name', value: user.first_name },
                 { property: 'last_name', value: user.last_name },
@@ -42,19 +42,19 @@ const populateBodies = deactivatedList => { //takes in array of users linked to 
                 { property: 'phone', value: user.phone }
             ]
         };
-        createBody.push(newUser);
+        createBody.push(newUser); //populate user creation body array 
     })
 }
 
 var userRequest = new XMLHttpRequest();
 userRequest.open("GET", "./data/users.json", false);
 userRequest.send(null)
-var users = JSON.parse(userRequest.responseText);
+var users = JSON.parse(userRequest.responseText); //array of user objects from JSON file
 
 var propRequest = new XMLHttpRequest();
 propRequest.open("GET", "./data/properties.json", false);
 propRequest.send(null)
-var properties = JSON.parse(propRequest.responseText);
+var properties = JSON.parse(propRequest.responseText); //array of property objects from JSON file
 
 const deactivatedList = users.filter(getMatchingUser); //array of users who are linked to deactivated properties for deposit 
 
@@ -62,7 +62,7 @@ const deactivatedProps = properties.filter(getDeactivatedProps); //array of deac
 
 populateBodies(deactivatedList); //used to set exported bodies for both api calls
 
-module.exports = { listBody, createBody }
+module.exports = { listBody, createBody };
 
 
 
